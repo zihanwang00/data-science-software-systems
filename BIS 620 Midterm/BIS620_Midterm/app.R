105fed9d6c4f867b49cb771e0c9e5bc5f3f20150
zihanwang00 <113872795+zihanwang00@users.noreply.github.com>
2023-11-02 13:48:40 -0700

    Create app.R

---
+++ BIS 620 Midterm/BIS620_Midterm/app.R
@@ -0,0 +1,112 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+source("ct-util.R")
+max_num_studies = 1000
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+  
+  # Application title
+  titlePanel("Clinical Trials Query"),
+  
+  # Sidebar with a slider input for number of bins 
+  sidebarLayout(
+    position = 'left',
+    sidebarPanel(
+      textInput("brief_title_kw", "Brief title keywords"),
+      checkboxGroupInput("source_class", 
+                         label = h3("Sponsor Type"),
+                         choices = list("Federal" = "FED", 
+                                        "Individual" = "INDIV", 
+                                        "Industry" = "INDUSTRY", 
+                                        "Network" = "NETWORK", 
+                                        "NTH" = "NTH",
+                                        "Other" = "OTHER", 
+                                        "Other gov" = "OTHER_GOV", 
+                                        "Unknown" = "Unknown")),
+    ),
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+      tabsetPanel(
+        type = "tabs",
+        tabPanel("Phase", plotOutput("phase_plot")),
+        tabPanel("Concurrent", plotOutput("concurrent_plot")),
+        tabPanel("Conditions", plotOutput("condition_plot"))
+      ),
+      dataTableOutput("trial_table")
+    )
+  )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+  # react to user input
+  # when user input change, get_studies will be updated, this will trigger other functions
+  get_studies = reactive({
+    if (input$brief_title_kw != "") {
+      si = input$brief_title_kw |>
+        strsplit(",") |>
+        unlist() |>
+        trimws()
+      ret = query_kwds(studies, si, "brief_title", match_all = TRUE)
+    } else {
+      ret = studies
+    }
+    
+    if (!is.null(input$source_class)) {
+      ret = ret |> 
+        filter(source_class %in% !!input$source_class) # '!!': use the value it's pointing to
+    }
+    ret |>
+      head(max_num_studies) |>
+      collect()
+  })
+  
+  
+  output$phase_plot = renderPlot({
+    get_studies() |>
+      plot_phase_histogram()
+  })
+  
+  output$concurrent_plot = renderPlot({
+    get_studies() |>
+      select(start_date, completion_date) |>
+      get_concurrent_trials() |>
+      ggplot(aes(x = date, y = count)) +
+      geom_line() +
+      xlab("Date") +
+      ylab("Count") + 
+      theme_bw()
+  })
+  
+  output$condition_plot = renderPlot({
+    get_studies()|>
+      get_num_condition() |>
+      ggplot(aes(x=name, y=n))+
+      geom_col()+
+      theme_bw()+
+      xlab("Condition")+
+      ylab("Count")
+  })
+  
+  output$trial_table = renderDataTable({
+    get_studies() |> 
+      select(nct_id, brief_title, start_date, completion_date) |>
+      rename(`NCT ID` = nct_id, `Brief Title` = brief_title,
+             `Start Date` = start_date, `Completion Date` = completion_date)
+  })
+  
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
