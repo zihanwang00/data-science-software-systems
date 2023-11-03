760c1451e08f816e860b6414faf39693411f211a
zihanwang00 <113872795+zihanwang00@users.noreply.github.com>
2023-11-02 19:39:28 -0700

    test whether feature 1 works

--- BIS 620 Midterm/BIS620_Midterm/test.R
+++ BIS 620 Midterm/BIS620_Midterm/test.R
@@ -1,3 +1,37 @@
 library(dplyr)
 library(tidyr)
+
 source("ct-util.R")
+
+
+# test for Feature 1
+library(countrycode)
+library(rnaturalearth)
+library(rnaturalearthdata)
+library(RColorBrewer)
+
+countries$name[is.na(countries$name)] = "NA"
+
+aggregated_data = countries |>
+  collect() |>
+  select(-removed) |>
+  group_by(name) |>
+  summarize(n=n()) |>
+  ungroup()
+aggregated_data$name <- countrycode(aggregated_data$name, "country.name", "iso3c")
+
+world <- ne_countries(scale = "medium", returnclass = "sf")
+map_data <- world |>
+  left_join(aggregated_data, by = c("iso_a3" = "name"))
+
+ggplot(map_data) +
+  geom_sf(aes(fill = n), color = "white", size = 0.2) +
+  scale_fill_continuous(
+    low = "lightblue", high = "darkblue",
+    na.value = "grey50",  # Color for countries with no data
+    name = "Number of Trials"
+  ) +
+  labs(title = "Trials per Country") +
+  theme_void()
+
+
