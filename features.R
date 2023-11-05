c10ccb6a79826c8baadd21efbe74fc220d512332
Kelly Wang <77018945+kellywang568@users.noreply.github.com>
2023-11-05 01:39:01 -0400

    Add files via upload
    Kelly edits 11.5.2023

---
+++ features.R
@@ -0,0 +1,19 @@
+# Feature 1: Create a world map of number of trials per country by a title keyword search
+#' @title World Map 
+#' @description 
+#' @motivation 
+#' @implementation
+#' 
+#' @param 
+#' @param 
+#' @param 
+
+# Feature 2: Create a condition keyword search to filter condition plot, update on condition plot
+#' @title Condition Keyword Search
+#' @description 
+#' @motivation 
+#' @implementation
+#' 
+#' @param 
+#' @param 
+#' @param 
