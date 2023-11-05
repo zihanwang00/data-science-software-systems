9dd48b1a6e1e54801b7e03ffb06219758c9965d2
Sara Shen <129796030+Sarashenstats@users.noreply.github.com>
2023-11-04 20:03:19 -0400

    Update app.R_deleteextra(

--- BIS 620 Midterm/BIS620_Midterm/app.R
+++ BIS 620 Midterm/BIS620_Midterm/app.R
@@ -150,7 +150,7 @@ server <- function(input, output) {
   output$outcomePieChart <- renderPlot({
     interventionTypes <- input$interventionType
     get_outcome_pie_for_intervention(interventionTypes)
-  }))
+  })
   
 }
 
